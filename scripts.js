f4349ce46b4ebd7ee383ab9610cf4dc296347b09
SergiEnguix <serenpex@gmail.com>
2024-12-09 05:31:59 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,17 +10,33 @@
         }
 
         // Detectar si la URL contiene el parámetro 'success'
-        const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('success') && urlParams.get('success') === '1') {
+        const urlParamsSuccess = new URLSearchParams(window.location.search);
+        if (urlParamsSuccess.has('success') && urlParamsSuccess.get('success') === '1') {
             // Mostrar el pop-up dinámico
-            const alertBox = document.getElementById('alert');
-            alertBox.style.display = 'block';
+            const alertBoxSuccess = document.getElementById('alertSuccess');
+            alertBoxSuccess.style.display = 'block';
 
             // Ocultar el pop-up después de 10 segundos
             setTimeout(function() {
-                alertBox.style.display = 'none';
+                alertBoxSuccess.style.display = 'none';
             }, 10000);
 
             // Eliminar el parámetro 'success' de la URL
+            history.replaceState(null, "", window.location.pathname);
+        }
+
+        // Detectar si la URL contiene el parámetro 'error'
+        const urlParamsFailure = new URLSearchParams(window.location.search);
+        if (urlParamsFailure.has('error') && urlParamsFailure.get('error') === '1') {
+            // Mostrar el pop-up dinámico
+            const alertBoxFailure = document.getElementById('alertFailure');
+            alertBoxFailure.style.display = 'block';
+
+            // Ocultar el pop-up después de 10 segundos
+            setTimeout(function() {
+                alertBoxFailure.style.display = 'none';
+            }, 10000);
+
+            // Eliminar el parámetro 'error' de la URL
             history.replaceState(null, "", window.location.pathname);
         }
